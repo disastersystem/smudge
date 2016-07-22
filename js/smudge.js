2f47562d51955f578e2cab074bafb3d2d34f8af0
disastersystem <robin.vigdal.bekkevold@gmail.com>
2016-07-22 18:02:43 +0200

    Shared saved shape array across instances.

--- js/smudge.js
+++ js/smudge.js
@@ -1,12 +1,16 @@
 (function() {
 
     this.Smudge = function(elems, options) {
+        this.shapes = [];
+
+        var self = this;
         forEach(document.querySelectorAll(elems), function(canvasIndex, element) {
-            initSmudge.apply(element, [canvasIndex, element, options]);
+            initSmudge.apply(element, [canvasIndex, element, options, self]);
+            self.shapes.push([]);
         });
     }
 
-    function initSmudge(canvasIndex, element, options) {
+    function initSmudge(canvasIndex, element, options, smudge) {
         'use strict';
 
         /* establish our default settings, override if any provided */
@@ -30,8 +34,6 @@
 
         /* here we will store the coordinates of the shape currently being drawn */
         var activeShape = [];
-        /* here we will store the coordinates of the finished shapes */
-        var savedShapes = [];
         /* here we will store any annotations the user adds to the finished shapes */
         var annotations = [];
 
@@ -175,7 +177,7 @@
             savedCtx.strokeStyle = 'rgba(0, 100, 0, 0.6)';
             savedCtx.lineWidth = 2;
 
-            savedShapes.forEach(function(shape) {
+            smudge.shapes[canvasIndex].forEach(function(shape) {
 
                 savedCtx.beginPath();
                 /* start drawing the shape from the first coordinate in the array */
@@ -210,7 +212,7 @@
 
             drawingCtx.lineWidth = 2;
 
-            savedShapes.forEach(function(shape) {
+            smudge.shapes[canvasIndex].forEach(function(shape) {
                 drawingCtx.beginPath();
                 /* start the shape at the first coordinate in the array */
                 drawingCtx.moveTo(shape.coords[0].x, shape.coords[0].y);
@@ -248,9 +250,7 @@
                 inputValue: ( (text != '') ? text : '' ),
                 /* save the annotation whenever the modal closes */
                 onClose: function(event) {
-                    saveAnnotation(
-                        id, inputModal.getInputValue()
-                    );
+                    saveAnnotation(id, inputModal.getInputValue());
                 }
             });
 
@@ -268,7 +268,7 @@
          */
         function saveAnnotation(id, text) {
 
-            savedShapes.forEach(function(shape) {
+            smudge.shapes[canvasIndex].forEach(function(shape) {
                 /*  */
                 if (shape.id == id) {
                     /* if an annotation has already been saved for this shape,
@@ -301,8 +301,8 @@
         function saveShape(e) {
             /* only save the shape if we have atleast 3 points */
             if (activeShape.length > 2) {
-                savedShapes.push({
-                    id: savedShapes.length + 1,
+                smudge.shapes[canvasIndex].push({
+                    id: smudge.shapes[canvasIndex].length + 1,
                     coords: activeShape
                 });
             }
@@ -322,7 +322,7 @@
          * @return void
          */
         function undo() {
-            (activeShape.length > 0) ? activeShape = [] : savedShapes.pop();
+            (activeShape.length > 0) ? activeShape = [] : smudge.shapes[canvasIndex].pop();
 
             draw();
             drawSavedShapes();
@@ -336,16 +336,13 @@
          */
         function reset() {
             activeShape = [];
-            savedShapes = [];
+            smudge.shapes[canvasIndex] = [];
 
             draw();
             drawSavedShapes();
         }
 
     } /* end of initSmudge function */
-
-
-
 
     /* Extend defaults with user options. */
     function extendDefaults(source, properties) {
@@ -358,7 +355,8 @@
         return source;
     }
 
-    /* forEach method, could be shipped as part of an Object Literal/Module */
+    /* forEach method for iterating thorugh NodeLists.
+     * Such as the one returned from querySelectorAll */
     function forEach(array, callback, scope) {
         for (var i = 0; i < array.length; i++) {
             /* passes back stuff we need */
@@ -367,3 +365,214 @@
     }
 
 })();
+
+(function() {
+
+    /**
+     * Define our constructor.
+     * The 'this' keyword is pointing to the window object, which means we're
+     * attaching our constructor to the global scope.
+     */
+    this.AnnotationModal = function() {
+        'use strict';
+
+        /* Create global element references */
+        this.modal = null;
+        this.annotationForm = null;
+        this.annotationInput = null;
+        this.overlay = null;
+
+        /* Define option defaults */
+        var defaults = {
+            className: 'fade-and-drop',
+            content: '',
+            inputValue: '',
+            maxWidth: 400,
+            minWidth: 280
+        }
+
+        /**
+         * The arguments object. This is a magical object inside of every function that contains
+         * an array of everything passed to it via arguments. Because we are only expecting
+         * one argument, an object containing plugin settings, we check to make sure arguments[0]
+         * exists, and that it is indeed an object.
+         * -------------------
+         * create options by extending defaults with the passed in arguments
+         */
+        if (arguments[0] && typeof arguments[0] === "object") {
+            /* merge the two objects using a privately scoped utility method called extendDefaults */
+            this.options = extendDefaults(defaults, arguments[0]);
+        }
+    }
+
+    /* Extend defaults with user options. */
+    function extendDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+    }
+
+
+    /**
+     * Open the modal.
+     */
+    AnnotationModal.prototype.open = function() {
+        /*
+         * Build out our Modal.
+         * Call our createMarkup method using the call method, similarly to the way we
+         * did in our event binding with bind. We are simply passing the proper value of this to the method.
+         */
+        createMarkup.call(this);
+
+        /* Initialize our event listeners */
+        initializeEvents.call(this);
+
+        /*
+         * After adding elements to the DOM, use getComputedStyle
+         * to force the browser to recalc and recognize the elements
+         * that we just added. This is so that CSS animation has a start point
+         */
+        window.getComputedStyle(this.modal).height;
+
+        /*
+         * Add our open class and check if the modal is taller than the window.
+         * If so, our anchored class is also applied.
+         */
+        this.modal.className =
+            this.modal.className + (this.modal.offsetHeight > window.innerHeight ?
+                " annotation-modal-open annotation-modal-anchored" : " annotation-modal-open");
+
+        this.overlay.className = this.overlay.className + " annotation-modal-open";
+    }
+
+    /**
+     * Close the modal by removing the nodes from the DOM when the CSS animation has ended.
+     */
+    AnnotationModal.prototype.close = function(event) {
+        /* stop the form from submitting */
+        event.preventDefault();
+
+        /* Store the current value of this */
+        var self = this;
+
+        removeEvents.call(this);
+
+        /* Remove the open class name */
+        this.modal.className = this.modal.className.replace(" annotation-modal-open", "");
+        this.overlay.className = this.overlay.className.replace(" annotation-modal-open", "");
+
+        /*
+         * Listen for CSS transitionend event and then
+         * Remove the nodes from the DOM
+         */
+        this.modal.addEventListener('transitionend', function() {
+            self.modal.parentNode.removeChild(self.modal);
+        });
+
+        this.overlay.addEventListener('transitionend', function() {
+            if (self.overlay.parentNode) {
+                self.overlay.parentNode.removeChild(self.overlay);
+            }
+        });
+
+        /* trigger the  */
+        if (this.options.hasOwnProperty('onClose')) {
+            this.options.onClose();
+        }
+    }
+
+    /**
+     * Public method for getting the value of the input text field.
+     */
+    AnnotationModal.prototype.getInputValue = function() {
+        return this.annotationInput.value;
+    }
+
+    /**
+     * Build the HTML for the modal.
+     */
+    function createMarkup() {
+
+        /* Create a DocumentFragment to build with */
+        var docFrag = document.createDocumentFragment();
+
+        /* Create modal element */
+        this.modal = document.createElement("div");
+        this.modal.className = "annotation-modal " + this.options.className;
+        this.modal.style.minWidth = this.options.minWidth + "px";
+        this.modal.style.maxWidth = this.options.maxWidth + "px";
+
+        /* Create overlay */
+        this.overlay = document.createElement("div");
+        this.overlay.className = "annotation-modal-overlay " + this.options.classname;
+        docFrag.appendChild(this.overlay);
+
+        /* Create content area */
+        var contentHolder = document.createElement("div");
+        contentHolder.className = "annotation-modal-content";
+
+        /* Create form */
+        this.annotationForm = document.createElement("form");
+        this.annotationForm.className = 'annotation-modal-form';
+
+        /* Create form input */
+        this.annotationInput = document.createElement("input");
+        this.annotationInput.setAttribute("type", "text");
+        this.annotationInput.setAttribute("placeholder", "What do you see?");
+        this.annotationInput.value = this.options.inputValue;
+        this.annotationInput.className = 'annotation-modal-input';
+
+        /* Append input to form */
+        this.annotationForm.appendChild(this.annotationInput);
+
+        /* Append form to contet area */
+        contentHolder.appendChild(this.annotationForm);
+
+        /* Append content area to modal */
+        this.modal.appendChild(contentHolder);
+
+        /* Append modal to DocumentFragment */
+        docFrag.appendChild(this.modal);
+
+        /* Append DocumentFragment to body */
+        document.body.appendChild(docFrag);
+
+        /* automatically put mouse focus inside the input field */
+        this.annotationInput.focus();
+    }
+
+
+    function initializeEvents(event) {
+        this.overlay.addEventListener(
+            /*
+             * Call close by using the bind method, which makes sure
+             * this will reference the same thing inside our close method,
+             * which is our modal object.
+             */
+            'click', this.close.bind(this)
+        );
+
+        this.annotationForm.addEventListener(
+            'submit', this.close.bind(this)
+        );
+    }
+
+    /**
+     * Removes event listeners to avoid duplication when the modal
+     * is opened and closed multiple times.
+     */
+    function removeEvents(event) {
+        this.overlay.removeEventListener(
+            'click', this.close.bind(this)
+        );
+
+        this.annotationForm.removeEventListener(
+            'submit', this.close.bind(this)
+        );
+    }
+
+}());
